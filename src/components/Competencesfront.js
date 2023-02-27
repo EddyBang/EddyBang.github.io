9c52df264ab194b455620778b90d5369a83045e2
David LI <108392457+BanggEddy@users.noreply.github.com>
2023-02-27 14:23:07 +0100

    add certifs

--- src/components/Competencesfront.js
+++ src/components/Competencesfront.js
@@ -1,152 +1,257 @@
-import React from 'react';
+import React from "react";
 
 const Competencefront = () => {
-    return (
-        <div>
-            <div className="affich" style={{height : "1100px"}}>
-            <div className="centrer">
-            <div className="container">
-    <div className="moi" style={{height: "120px",  textAlign: "center"}}>
-        <d><b style={{fontSize:"20px"}}> Mes Compétences
-            </b></d>
-    </div>
-    <div class="row">
-          {/*Développements */}
-        <div class="col">
-            <p style={{fontSize: "20px",height: "60px",fontWeight:"bold"}}>Développements</p>
-            <div class="row">
-                <div class="col">
-                    <img src="./vscode.png" style={{width : "100px", height : "100px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./vsmicrosoft.png" style={{width : "180px", height : "110px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./php.png" style={{ width : "120px",height : "100px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./github.png" style={{ width : "120px", height : "120px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./git.png" style={{ width : "100px", height : "100px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./bootstrap.png" style={{ width : "190px", height : "140px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./html.png" style={{ width : "120px", height : "110px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./mysql.png" style={{ width : "140px", height : "120px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./windesign.png" style={{ width : "140px", height : "120px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./css.png" style={{ width : "120px", height : "120px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./csharp.png" style={{ width : "120px", height : "110px" }}></img>
-                </div>
-                <div class="col">
-                    <img src="./java.png" style={{width : "100px", height : "100px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./python.png" style={{width : "120px", height : "110px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./blender.png" style={{width : "120px", height : "110px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./nodejs.png" style={{width : "180px", height : "110px"}}></img>
-                </div>
-                                <div class="col">
-                    <img src="./mariadb.png" style={{width : "350px", height : "110px"}}></img>
-                </div>
-                                <div class="col">
-                    <img src="./react.png" style={{width : "115px", height : "100px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./mongodb.png" style={{width : "120px", height : "110px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./linux.png" style={{width : "120px", height : "110px"}}></img>
-                </div>
-                <div class="col">
-                    <img src="./symfony.png" style={{width : "110px", height : "120px"}}></img>
-                </div>
+  return (
+    <div>
+      <div className="affich" style={{ height: "1100px" }}>
+        <div className="centrer">
+          <div className="container">
+            <div
+              className="moi"
+              style={{ height: "120px", textAlign: "center" }}
+            >
+              <d>
+                <b style={{ fontSize: "20px" }}> Mes Compétences</b>
+              </d>
             </div>
-
-            <br></br><br></br><br></br>
-
-
-        </div>
-
-        {/*Réseaux/Cybersécurité*/}
-        <div className="row">
-        <div class="col">
-            <p style={{fontSize: "20px",height: "60px",fontWeight:"bold"}}>Réseaux/Cybersécurités</p>
             <div class="row">
+              {/*Développements */}
+              <div class="col">
+                <p
+                  style={{
+                    fontSize: "20px",
+                    height: "60px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Développements
+                </p>
+                <div class="row">
+                  <div class="col">
+                    <img
+                      src="./vscode.png"
+                      style={{ width: "100px", height: "100px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./vsmicrosoft.png"
+                      style={{ width: "180px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./php.png"
+                      style={{ width: "150px", height: "100px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./github.png"
+                      style={{ width: "120px", height: "120px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./git.png"
+                      style={{ width: "100px", height: "100px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./bootstrap.png"
+                      style={{ width: "190px", height: "140px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./html.png"
+                      style={{ width: "120px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./mysql.png"
+                      style={{ width: "140px", height: "120px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./windesign.png"
+                      style={{ width: "140px", height: "120px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./css.png"
+                      style={{ width: "120px", height: "120px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./csharp.png"
+                      style={{ width: "120px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./java.png"
+                      style={{ width: "100px", height: "100px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./python.png"
+                      style={{ width: "120px", height: "110px" }}
+                    ></img>
+                  </div>
+
+                  <div class="col">
+                    <img
+                      src="./nodejs.png"
+                      style={{ width: "180px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./mariadb.png"
+                      style={{ width: "350px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./react.png"
+                      style={{ width: "115px", height: "100px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./mongodb.png"
+                      style={{ width: "120px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./linux.png"
+                      style={{ width: "120px", height: "110px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./symfony.png"
+                      style={{ width: "110px", height: "120px" }}
+                    ></img>
+                  </div>
+                  <div class="col">
+                    <img
+                      src="./sass.png"
+                      style={{ width: "100px", height: "100px" }}
+                    ></img>
+                  </div>
+                </div>
+
+                <br></br>
+                <br></br>
+                <br></br>
+              </div>
+
+              {/*Réseaux/Cybersécurité*/}
+              <div className="row">
                 <div class="col">
-                    <img src="./rgpd.png" style={{width : "140px", height : "100px"}}></img>
+                  <p
+                    style={{
+                      fontSize: "20px",
+                      height: "60px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Réseaux/Cybersécurités
+                  </p>
+                  <div class="row">
+                    <div class="col">
+                      <img
+                        src="./rgpd.png"
+                        style={{ width: "140px", height: "100px" }}
+                      ></img>
+                    </div>
+                    <div class="col">
+                      <img
+                        src="./cnil.png"
+                        style={{ width: "140px", height: "100px" }}
+                      ></img>
+                    </div>
+                    <div class="col">
+                      <img
+                        src="./cisco.png"
+                        style={{ width: "140px", height: "100px" }}
+                      ></img>
+                    </div>
+                  </div>
+
+                  <br></br>
+                  <br></br>
+                  <br></br>
                 </div>
                 <div class="col">
-                    <img src="./cnil.png" style={{width : "140px", height : "100px"}}></img>
+                  <p
+                    style={{
+                      fontSize: "20px",
+                      height: "60px",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Langues
+                  </p>
+                  <div class="row">
+                    <div class="col">Anglais-Niveau B1</div>
+                    <div class="col">Mandarin-Niveau C1</div>
+                  </div>
                 </div>
-                <div class="col" >
-                    <img src="./cisco.png" style={{width : "140px", height : "100px"}}></img>
-                </div>
+              </div>
             </div>
-
-            <br></br><br></br><br></br>
-
-
-        </div>
-        <div class="col">
-            <p style={{fontSize: "20px",height: "60px",fontWeight:"bold"}}>Langues</p>
-            <div class="row">
-                <div class="col">
-                
-                    Anglais-Niveau B1
+            <div className="row">
+              <div className="container">
+                {" "}
+                <p
+                  style={{
+                    fontSize: "20px",
+                    height: "60px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Certificats
+                </p>
+                <div className="row">
+                  <div className="col">
+                    <img src="googlecertif.png" style={{ width: "100%" }}></img>
+                  </div>
+                  <div className="col">
+                    <img src="certifgoogle.png" style={{ width: "100%" }}></img>
+                  </div>
                 </div>
-                <div class="col">
-                    Mandarin-Niveau C1
-                </div>
-
-
+              </div>
             </div>
-
-        </div>
-
-
-
-
-        </div>
-
-    </div>
-    
-
-    </div>        
-    <div className="row">
-    <p style={{fontWeight:"bold",fontSize: "12px",height: "70px"}}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="" height="16" fill="currentColor" class="bi bi-c-circle" viewBox="0 0 16 16">
-                <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z"/>
+          </div>
+          <br></br> <br></br> <br></br>
+          <div className="row">
+            <p style={{ fontWeight: "bold", fontSize: "12px", height: "70px" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width=""
+                height="16"
+                fill="currentColor"
+                class="bi bi-c-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z" />
               </svg>
               2022 David LI. All right Reserved
             </p>
-
-    </div>
-
-
-</div>
-
-
-
-    </div>
-
-
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Competencefront;
